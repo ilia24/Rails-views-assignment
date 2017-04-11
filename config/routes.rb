d50d0fe822d0281811137369225f3eb3f70b4d4a
ilia24 <iliademertchian24@gmail.com>
2017-04-11 18:18:20 -0400

    Create events edit and add functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'example/form' => 'example#form'
   resources :job_applications
+  delete 'events/:id' => 'events#destroy', as delete
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
